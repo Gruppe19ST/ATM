120259b9aa7ebd21a0c4f0cd0ed994acf1ed620f
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-04-03 09:53:23 +0200

    Test af DLL

--- TransponderRecieverConsoleApp/Program.cs
+++ TransponderRecieverConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TransponderReceiver;
 
 namespace TransponderRecieverConsoleApp
 {
@@ -10,6 +11,29 @@ namespace TransponderRecieverConsoleApp
     {
         static void Main(string[] args)
         {
+            
+            var myReceiver = TransponderReceiverFactory.CreateTransponderDataReceiver();
+
+            myReceiver.TransponderDataReady += MyReceiver_TransponderDataReady;
+
+            Console.ReadLine();
+
         }
+
+        private static void MyReceiver_TransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            var receiverData = e.TransponderData;
+            PrintData(receiverData);
+        }
+
+        private static void PrintData(List<string> data)
+        {
+            foreach (var d in data)
+            {
+                Console.WriteLine(d);
+            }
+        }
+
+        
     }
 }
