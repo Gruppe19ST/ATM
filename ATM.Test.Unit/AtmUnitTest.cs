93cfd69bd3491b7a05057d484f5023791bbac36d
ChristinaLund <201505829@post.au.dk>
2018-04-09 11:01:43 +0200

    Unit tests

--- ATM.Test.Unit/AtmUnitTest.cs
+++ ATM.Test.Unit/AtmUnitTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +24,66 @@ namespace ATM.Test.Unit
             _uut = new TrackConverter(_transponderReceiver);
         }
 
-        //[Test]
+        [Test]
+        public void stringToTrack_Conversion_TagIsCorrect()
+        {
+            string input = "Tag123;12345;12345;1000;20151006213456789";
+            TrackObject outputTrack = _uut.ConvertTrackObject(input);
+            Assert.That(outputTrack.Tag,Is.EqualTo("Tag123"));
+        }
+
+        [Test]
+        public void stringToTrack_Conversion_XcorIsCorrect()
+        {
+            string input = "Tag123;12345;12345;1000;20151006213456789";
+            TrackObject outputTrack = _uut.ConvertTrackObject(input);
+            Assert.That(outputTrack.XCoordinate, Is.EqualTo(12345));
+        }
+        [Test]
+        public void stringToTrack_Conversion_YcorIsCorrect()
+        {
+            string input = "Tag123;12345;12345;1000;20151006213456789";
+            TrackObject outputTrack = _uut.ConvertTrackObject(input);
+            Assert.That(outputTrack.YCoordinate, Is.EqualTo(12345));
+        }
+        [Test]
+        public void stringToTrack_Conversion_AltIsCorrect()
+        {
+            string input = "Tag123;12345;12345;1000;20151006213456789";
+            TrackObject outputTrack = _uut.ConvertTrackObject(input);
+            Assert.That(outputTrack.Altitude, Is.EqualTo(1000));
+        }
+
+        [Test]
+        public void stringToTrack_Conversion_TimestampIsCorrect()
+        {
+            string input = "Tag123;12345;12345;1000;20151006213456789";
+            TrackObject outputTrack = _uut.ConvertTrackObject(input);
+            Assert.That(outputTrack.TimeStamp, Is.EqualTo(DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture)));
+        }
+
+
+
+
+        //[Test] // Fra Emma
+        //public void String_to_Track()
+        //{
+        //    List<String> trackInfo = new List<string> {"Tag123;12345;12345;1000;20151006213456789"};
+
+        //    TrackObject track = new TrackObject()//mangler parametre
+        //    {
+        //        Tag = "Tag123",
+        //        XCoordinate = 12345,
+        //        YCoordinate = 12345,
+        //        Altitude = 1000,
+        //        TimeStamp = new DateTime(2015, 10, 06, 21, 34, 56, 789)
+        //    };
+
+        //    _uut.ConvertTrackObject(trackInfo);
+        //    Assert.That(_uut.Tracks[0], is.TypeOf<TrackObject>());
+        //}
+
+
 
     }
 }
--- ATM/TrackConverter.cs
+++ ATM/TrackConverter.cs
@@ -24,6 +24,8 @@ namespace ATM
             {
                 TrackObject track = ConvertTrackObject(data); // Opretter et nyt track for hvert element i listen
                 Console.WriteLine(track.ToString()); // udskriver track i konsolvinduet 
+
+                // Add til en liste af Tracks? Så vi kan på et tidspunkt kan holde styr på det enkelte? 
             }
         }
 
@@ -34,11 +36,10 @@ namespace ATM
             float xCoordinate = float.Parse(elements[1]);
             float yCoordinate = float.Parse(elements[2]);
             float altitude = float.Parse(elements[3]);
-            // string timeStamp = elements[4]; // skal ændres til en datetime senere, så output bliver læseligt. 
             DateTime timeStamp = DateTime.ParseExact(elements[4], "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
             // https://msdn.microsoft.com/en-us/library/w2sa9yss(v=vs.110).aspx
             return new TrackObject(tag, xCoordinate, yCoordinate, altitude, timeStamp); // returnerer nyt track
         }
-
+         
     }
 }
