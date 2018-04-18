c885fb6540c04bb306f6874d2561aa2d992c644b
Emmapalmkvist <palmkvistemma@hotmail.com>
2018-04-18 17:04:42 +0200

    TrackCourse - unitest

--- ATM.Test.Unit/TrackCourseUnitTest.cs
+++ ATM.Test.Unit/TrackCourseUnitTest.cs
@@ -14,26 +14,61 @@ namespace ATM.Test.Unit
     class TrackCourseUnitTest
     {
         private ATM.Logic.Handlers.TrackCompassCourse _uut;
-
-        
         private TrackObject track1;
         private TrackObject track2;
+        private double trackcoure;
 
         [SetUp]
 
         public void Setup()
         {
             _uut = new ATM.Logic.Handlers.TrackCompassCourse();
+            trackcoure = 0;
+
         }
 
-        [TestCase("Hej", 10000, 15000, 10000, 15000, 315)]
-        [TestCase("Hej", 10000, 5000, 10000, 15000, 45)]
-        [TestCase("Hej", 10000, 5000, 10000, 5000, 135)]
-        [TestCase("Hej", 1000, 15000, 10000, 5000, 225)]
-        public void CourseTest()
+        [Test]
+        public void CalculateCoursePositiveXandY()
         {
-            _uut.CalculateCompassCourse(track1, track2);
+            track1 = new TrackObject("Tag123", 80000, 60000, 1000, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            track2 = new TrackObject("Tag123", 80500, 60400, 1000, DateTime.ParseExact("20180412111322111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            trackcoure = _uut.CalculateCompassCourse(track1, track2);
+
+            Assert.That(Math.Round(trackcoure), Is.EqualTo(Math.Round(0)));
         }
+
+        [Test]
+        public void CalculateCourseNegativeXandY()
+        {
+            track1 = new TrackObject("Tag123", 80000, 60000, 1000, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            track2 = new TrackObject("Tag123", 70500, 50400, 1000, DateTime.ParseExact("20180412111322111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            trackcoure = _uut.CalculateCompassCourse(track1, track2);
+
+            Assert.That(Math.Round(trackcoure), Is.EqualTo(Math.Round(0)));
+        }
+
+        [Test]
+        public void CalculateCoursePositiveXandNegativY()
+        {
+            track1 = new TrackObject("Tag123", 80000, 60000, 1000, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            track2 = new TrackObject("Tag123", 81500, 58000, 1000, DateTime.ParseExact("20180412111322111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            trackcoure = _uut.CalculateCompassCourse(track1, track2);
+
+            Assert.That(Math.Round(trackcoure), Is.EqualTo(Math.Round(0)));
+        }
+
+        [Test]
+        public void CalculateCourseNegativeXandPostiveY()
+        {
+            track1 = new TrackObject("Tag123", 80000, 60000, 1000, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            track2 = new TrackObject("Tag123", 70500, 62000, 1000, DateTime.ParseExact("20180412111322111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            trackcoure = _uut.CalculateCompassCourse(track1, track2);
+
+            Assert.That(Math.Round(trackcoure), Is.EqualTo(Math.Round(0)));
+        }
+
+
+
 
     }
 }
