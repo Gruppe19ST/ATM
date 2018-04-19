218d84ac3145c8b4ebe33605d35f9e0af63ece30
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-04-19 12:05:37 +0200

    SeparationEvent

--- ATM/Logic/Handlers/CheckForSeparationEvent.cs
+++ ATM/Logic/Handlers/CheckForSeparationEvent.cs
@@ -13,6 +13,10 @@ namespace ATM.Logic.Handlers
         // List to hold received list of tracks
         private readonly List<TrackObject> _listOfTracks;
 
+        private List<SeparationEventObject> _listOfSeparations;
+        private List<SeparationEventObject> _currentSeparations;
+        private List<SeparationEventObject> _priorSeparations;
+
         // Variables for the limits for horizontal and vertical separation
         private readonly float _horizontalSeperationLimit, _verticalSeperationLimit;
 
@@ -20,6 +24,9 @@ namespace ATM.Logic.Handlers
         {
             // Initialization of lists
             _listOfTracks = new List<TrackObject>();
+            _listOfSeparations = new List<SeparationEventObject>();
+            _currentSeparations = new List<SeparationEventObject>();
+            _priorSeparations = new List<SeparationEventObject>();
 
             // Set the limits to the specified values
             _horizontalSeperationLimit = 5000;
@@ -31,6 +38,8 @@ namespace ATM.Logic.Handlers
 
         public void CheckSeparationEvents()
         {
+            _listOfSeparations.Clear();
+
             // Runs through the elements in _listOfTracks
             for (int i = 0; i < _listOfTracks.Count; i++)
             {
@@ -42,12 +51,51 @@ namespace ATM.Logic.Handlers
                         && Math.Abs(_listOfTracks[i].YCoordinate - _listOfTracks[j].YCoordinate) <= _horizontalSeperationLimit
                         && Math.Abs(_listOfTracks[i].Altitude - _listOfTracks[j].Altitude) <= _verticalSeperationLimit)
                     {
-                        // If two tracks are on separation course, they should be added to a list with TrackObjects
+                        // If two tracks are on separation course, an eventobject is created and added to list
+                        SeparationEventObject separationEvent = new SeparationEventObject(_listOfTracks[i].Tag, _listOfTracks[j].Tag, _listOfTracks[i].TimeStamp);
+                        _listOfSeparations.Add(separationEvent);
+
+                       /* // If two tracks are on separation course, they should be added to a list with TrackObjects
                         // And this list should be added to the conflicted-list
-                        OnSeparationEvent(new SeparationEventArgs(new List<TrackObject> { _listOfTracks[i], _listOfTracks[j] }));
+                        OnSeparationEvent(new SeparationEventArgs(new List<TrackObject> { _listOfTracks[i], _listOfTracks[j] }));*/
                     }
                 }
             }
+
+            // Compare _listOfSeparations to priorSeparations
+            foreach (var newSep in _listOfSeparations)
+            {
+                foreach (var priorSep in _priorSeparations)
+                {
+                    if ((newSep.Tag1 == priorSep.Tag1 && newSep.Tag2 == priorSep.Tag2)
+                        || newSep.Tag1 == priorSep.Tag2 && newSep.Tag2 == priorSep.Tag1)
+                    {
+                        priorSep.EventTime = newSep.EventTime;
+                        _listOfSeparations.Remove(newSep);
+                    }
+                    else
+                    {
+                        _priorSeparations.Add(newSep);
+                    }
+                }
+            }
+
+            foreach (var priorSep in _priorSeparations)
+            {
+                foreach (var newSep in _listOfSeparations)
+                {
+                    if ((priorSep.Tag1 == newSep.Tag1 && priorSep.Tag2 == newSep.Tag2
+                         || priorSep.Tag1 == newSep.Tag2 && priorSep.Tag2 == newSep.Tag1))
+                    {
+                        priorSep.EventTime = newSep.EventTime;
+                    }
+                    else
+                    {
+                        _priorSeparations.Remove(priorSep);
+                    }
+                }
+            }
+
         }
 
         private void OnSeparationEvent(SeparationEventArgs conflictedList)
---
+++ ATM/Logic/SeparationEventObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Logic
+{
+    public class SeparationEventObject
+    {
+        public string Tag1 { get; set; }
+        public string Tag2 { get; set; }
+        public DateTime EventTime { get; set; }
+
+        public SeparationEventObject(string tag1, string tag2, DateTime eventTime)
+        {
+            Tag1 = tag1;
+            Tag2 = tag2;
+            EventTime = eventTime;
+        }
+
+    }
+}
