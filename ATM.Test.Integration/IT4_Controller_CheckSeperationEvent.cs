c8c9a867fb8d5de0466ca62afd81a930d5e44d85
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-04-24 08:20:31 +0200

    IT4 and IT5

--- ATM.Test.Integration/IT4_Controller_CheckSeperationEvent.cs
+++ ATM.Test.Integration/IT4_Controller_CheckSeperationEvent.cs
@@ -11,6 +11,7 @@ using NSubstitute;
 using NSubstitute.Core.Arguments;
 using NUnit.Framework;
 using NUnit.Framework.Api;
+using TransponderReceiver;
 
 namespace ATM.Test.Integration
 {
@@ -18,12 +19,13 @@ namespace ATM.Test.Integration
     class IT4_Controller_CheckSeperationEvent
     {
         #region Defining objects
-        // Driver/included
+        // Drivers/included
+        private ITransponderReceiver _receiver;
         private ITrackConverter _trackConverter;
         private ISorter _sorter;
 
         // System under test
-        private Controller _controller;
+        private ITrackController _controller;
         private ISeperationEventChecker _checker;
 
         // Stubs/mocks
@@ -31,73 +33,96 @@ namespace ATM.Test.Integration
         private ISeperationEventHandler _warningCreator;
         private ITrackSpeed _ts;
         private ITrackCompassCourse _tcc;
-        
 
         // Data
-        private List<TrackObject> _listOfTracks;
-        private TrackObject _track1, _track2, _track3;
+        private RawTransponderDataEventArgs _fakeRawArgs;
         private SeparationEventArgs _separationArgs;
-        private object _objectSender;
+        private SeparationEventArgs _finishedSeparationArgs;
 
+        /*
+        private List<TrackObject> _listOfTracks;
+        private TrackObject _track1, _track2, _track3;*/
         #endregion
 
+        #region Setup
         [SetUp]
         public void Setup()
         {
             // Drivers/included
-            _trackConverter = Substitute.For<ITrackConverter>();
+            _receiver = Substitute.For<ITransponderReceiver>();
+            _trackConverter = new TrackConverter(_receiver);
             _sorter = new Sorter(_trackConverter);
-
-
+            
             // System under test
             _checker = new CheckForSeparationEvent();
 
             // Stubs/mocks
             _logger = Substitute.For<ISeperationEventLogger>();
             _warningCreator = Substitute.For<ISeperationEventHandler>();
+            _ts = Substitute.For<ITrackSpeed>(); 
+            _tcc = Substitute.For<ITrackCompassCourse>(); 
+
 
             // System under test
             _controller = new Controller(_sorter, _ts, _tcc, _checker, _warningCreator, _logger);
             
-            _listOfTracks = new List<TrackObject>();
+            _fakeRawArgs = new RawTransponderDataEventArgs(new List<string>()
+            {
+                "Tag123;70000;70000;1000;20180420222222222","Tag456;68000;68000;800;20180420222222222", "Tag789;89000;89000;5000;20180420222222222"
+            });
 
-            _track1 = new TrackObject("Tag123", 70000, 70000, 1000, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
+            /*_track1 = new TrackObject("Tag123", 70000, 70000, 1000, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
             _track2 = new TrackObject("Tag456", 68000, 68000, 800, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
             _track3 = new TrackObject("Tag789", 89000, 89000, 5000, DateTime.ParseExact("20180412111111111", "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture));
             _listOfTracks.Add(_track1);
             _listOfTracks.Add(_track2);
-            _listOfTracks.Add(_track3);
+            _listOfTracks.Add(_track3);*/
 
             _checker.SeperationEvents += _checker_SeperationEvents;
+            _checker.FinishedSeperationEvents += _checker_FinishedSeperationEvents;
 
         }
 
         private void _checker_SeperationEvents(object sender, SeparationEventArgs e)
         {
-            _objectSender = sender;
             _separationArgs = e;
         }
 
-
-        #region CreateWarning
-
-        [Test]
-        public void HandleTrack_CheckSeparationEvents_CreateWarningReceiveEvent()
+        private void _checker_FinishedSeperationEvents(object sender, SeparationEventArgs e)
         {
+            _finishedSeparationArgs = e;
+        }
+        #endregion
 
-            // When the method is called, it calls CheckSeparationEvents on the _checker-class
-            _controller.CheckTracks(_listOfTracks);
-           
-            // _checker.CheckSeparationEvents(_listOfTracks);
+        #region Tests
 
-            // CheckSeparationEvents raises an event with args (=_separationArgs)
-            // _warningCreator assigns to this event and handles the information
-
+        // Test that an event indicating new separation information is raised
+        [Test]
+        public void HandleTrack_CheckSeparationEvents_RaiseSeparationEvent()
+        {
+            _receiver.TransponderDataReady += Raise.EventWith(_fakeRawArgs);
             Assert.That(_separationArgs.SeparationObjects.Count, Is.EqualTo(1));
         }
 
-        
+        // Test that an event indicating finished separations is raised
+        [Test]
+        public void HandleTrack_FinishedSeparationEvents_RaiseFinishedEvent()
+        {
+            _receiver.TransponderDataReady += Raise.EventWith(_fakeRawArgs);
+
+            _fakeRawArgs = new RawTransponderDataEventArgs(new List<string>()
+            {
+                "Tag123;70000;70000;1000;20180420222222222",
+                "Tag456;64000;64000;800;20180420222222222",
+                "Tag789;89000;89000;5000;20180420222222222"
+            });
+
+            _receiver.TransponderDataReady += Raise.EventWith(_fakeRawArgs);
+
+            Assert.That(_finishedSeparationArgs.SeparationObjects.Count ,Is.EqualTo(1));
+        }
 
         #endregion
+    
     }
 }
--- ATM.Test.Integration/IT5_CheckSeparation_Logfile_CreateWarning.cs
+++ ATM.Test.Integration/IT5_CheckSeparation_Logfile_CreateWarning.cs
@@ -3,10 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ATM.Logic.Controllers;
+using ATM.Logic.Handlers;
+using ATM.Logic.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
 
 namespace ATM.Test.Integration
 {
-    class IT5_CheckSeparation_Logfile_CreateWarning
+    [TestFixture]
+    public class IT5_CheckSeparation_Logfile_CreateWarning
     {
+        #region Defining objects
+        // Drivers/included
+        private ITransponderReceiver _receiver;
+        private ITrackConverter _trackConverter;
+        private ISorter _sorter;
+        private ITrackController _controller;
+        private ISeperationEventChecker _checker;
+
+        // System under test
+        private ISeperationEventLogger _logger;
+        private ISeperationEventHandler _warningCreator;
+
+        // Stubs/mocks
+        private ITrackSpeed _ts;
+        private ITrackCompassCourse _tcc;
+
+        // Data
+        private RawTransponderDataEventArgs _fakeRawArgs;
+        private SeparationEventArgs _separationArgs;
+        private SeparationEventArgs _finishedSeparationArgs;
+        #endregion
+
+        #region Setup
+        [SetUp]
+        public void Setup()
+        {
+            // Drivers/included
+            _receiver = Substitute.For<ITransponderReceiver>();
+            _trackConverter = new TrackConverter(_receiver);
+            _sorter = new Sorter(_trackConverter);
+            _checker = new CheckForSeparationEvent();
+
+
+            // System under test
+            _logger = new LogSeparationEvent(_checker);
+            _warningCreator = new CreateWarning(_checker);
+
+            // Stubs/mocks
+            _ts = Substitute.For<ITrackSpeed>(); 
+            _tcc = Substitute.For<ITrackCompassCourse>(); 
+
+            // Driver
+            _controller = new Controller(_sorter, _ts, _tcc, _checker, _warningCreator, _logger);
+
+            _fakeRawArgs = new RawTransponderDataEventArgs(new List<string>()
+            {
+                "Tag123;70000;70000;1000;20180420222222222","Tag456;68000;68000;800;20180420222222222", "Tag789;89000;89000;5000;20180420222222222"
+            });
+
+            _checker.SeperationEvents += _checker_SeperationEvents;
+            _checker.FinishedSeperationEvents += _checker_FinishedSeperationEvents;
+
+        }
+
+        private void _checker_SeperationEvents(object sender, SeparationEventArgs e)
+        {
+            _separationArgs = e;
+        }
+
+        private void _checker_FinishedSeperationEvents(object sender, SeparationEventArgs e)
+        {
+            _finishedSeparationArgs = e;
+        }
+        #endregion
     }
 }
